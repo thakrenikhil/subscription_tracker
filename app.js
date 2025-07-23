a03fe153efdfec608cbad415d424b3cde0ab0791
Nikhil <nikhilthakre459@gmail.com>
2025-07-24 01:54:02 +0530

    vercel configs

--- app.js
+++ app.js
@@ -1,37 +1,72 @@
+// import express from "express";
+// import { PORT } from "./config/env.js";
+// import userRouter from "./routes/user.routes.js";
+// import authRouter from "./routes/auth.routes.js";
+// import subscriptionRouter from "./routes/subscription.routes.js";
+// import connectToDatabase from "./db/mongodb.js";
+// import errorMiddleware from "./middlewares/error.middleware.js";
+// import cookieParser from "cookie-parser";
+// import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+// import workflowRouter from "./routes/workflow.routes.js";
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(errorMiddleware);
+// app.use(cookieParser());
+// app.use(arcjetMiddleware);
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/subscriptions", subscriptionRouter);
+// app.use("/api/v1/workflows", workflowRouter);
+// app._router.stack
+//     .filter(r => r.route)
+//     .map(r => r.route.path);
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     msg: "Welcome",
+//   });
+// });
+
+// app.listen(PORT,'0.0.0.0' ,async () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+
+//   await connectToDatabase();
+// });
+// export default app;
+
+
 import express from "express";
-import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-import connectToDatabase from "./db/mongodb.js";
+import workflowRouter from "./routes/workflow.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
-import workflowRouter from "./routes/workflow.routes.js";
+import connectToDatabase from "./db/mongodb.js";
+import serverless from "serverless-http";
+
+await connectToDatabase();
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(errorMiddleware);
 app.use(cookieParser());
 app.use(arcjetMiddleware);
+
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/workflows", workflowRouter);
-app._router.stack
-    .filter(r => r.route)
-    .map(r => r.route.path);
 
 app.get("/", (req, res) => {
-  res.json({
-    msg: "Welcome",
-  });
+  res.json({ msg: "Welcome from Vercel Serverless!" });
 });
 
-app.listen(PORT,'0.0.0.0' ,async () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.use(errorMiddleware);
 
-  await connectToDatabase();
-});
-export default app;
+export const handler = serverless(app);
